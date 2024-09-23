7a92644669da1d1b059d820f4b0f58d3d6a71108
DylanFromm08 <150657729+DylanFromm08@users.noreply.github.com>
2024-09-23 15:13:35 +0700

    Adding drink and food model

---
+++ lib/models/drink.dart
@@ -0,0 +1,7 @@
+class Drink {
+  final String name;
+  final String imageUrl;
+  final String description;
+
+  Drink({required this.name, required this.imageUrl, required this.description});
+}
---
+++ lib/models/food.dart
@@ -0,0 +1,7 @@
+class Food {
+  final String name;
+  final String imageUrl;
+  final String description;
+
+  Food({required this.name, required this.imageUrl, required this.description});
+}
